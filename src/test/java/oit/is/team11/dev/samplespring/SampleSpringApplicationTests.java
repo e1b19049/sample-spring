5433e066e37f37c3c19a78588e0012c0b5dd2fa6
e1b19049 <e1b19049@st.oit.ac.jp>
2021-10-05 14:29:01 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/team11/dev/samplespring/SampleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team11.dev.samplespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
